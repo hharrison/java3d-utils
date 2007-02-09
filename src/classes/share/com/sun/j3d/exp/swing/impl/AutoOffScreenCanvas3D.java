5781888c5cdc65e12a5f95bd555c96a42ab4482e
Kevin Rushforth <kcr@dev.java.net>
2007-02-09 17:20:46 +0000

    Update copyright to 2007
    git-svn-id: https://svn.java.net/svn/j3d-core-utils~svn/trunk@150 9497e636-51bd-65ba-982d-a4982e1767a5

--- src/classes/share/com/sun/j3d/exp/swing/impl/AutoOffScreenCanvas3D.java
+++ src/classes/share/com/sun/j3d/exp/swing/impl/AutoOffScreenCanvas3D.java
@@ -1,7 +1,7 @@
 /*
  * $RCSfile$
  *
- * Copyright (c) 2006 Sun Microsystems, Inc. All rights reserved.
+ * Copyright (c) 2007 Sun Microsystems, Inc. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
