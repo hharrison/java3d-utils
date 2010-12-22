9f2d34dc4dfdc70820cb75dbf5b841624ea8b3bc
Ken Mc Neill <ken@mc-neill.net>
2010-12-22 09:36:54 +0000

    j3dutils: cherry-pick fix from svn trunk issue 654
    issue 654 fixed by mcneillk
    
    git-svn-id: https://svn.java.net/svn/j3d-core-utils~svn/trunk@204 9497e636-51bd-65ba-982d-a4982e1767a5
    Signed-off-by: Harvey Harrison <harvey.harrison@gmail.com>

--- src/classes/share/com/sun/j3d/utils/scenegraph/io/state/javax/media/j3d/SpotLightState.java
+++ src/classes/share/com/sun/j3d/utils/scenegraph/io/state/javax/media/j3d/SpotLightState.java
@@ -45,37 +45,59 @@
 package com.sun.j3d.utils.scenegraph.io.state.javax.media.j3d;
 
 import java.io.*;
+
+import javax.media.j3d.PointLight;
 import javax.media.j3d.SpotLight;
+import javax.vecmath.Point3f;
 import javax.vecmath.Vector3f;
 import com.sun.j3d.utils.scenegraph.io.retained.Controller;
 import com.sun.j3d.utils.scenegraph.io.retained.SymbolTableData;
 
+// issue 654: should inherit from PointLightState, but too hard to refactor
 public class SpotLightState extends LightState {
 
-    public SpotLightState( SymbolTableData symbol, Controller control ) {
-	super( symbol, control );
+	public SpotLightState(SymbolTableData symbol, Controller control) {
+		super(symbol, control);
 
-    }
-    
-    public void writeObject( DataOutput out ) throws IOException {
-        super.writeObject( out );
-	Vector3f dir = new Vector3f();
-	((SpotLight)node).getDirection( dir );
-	control.writeVector3f( out, dir );
+	}
 
-	out.writeFloat( ((SpotLight)node).getSpreadAngle());
-	out.writeFloat( ((SpotLight)node).getConcentration());
-    }
+	public void writeObject(DataOutput out) throws IOException {
+		super.writeObject(out);
 
-    public void readObject( DataInput in ) throws IOException {
-        super.readObject( in );
-	((SpotLight)node).setDirection( control.readVector3f(in) );
-	((SpotLight)node).setSpreadAngle( in.readFloat() );
-	((SpotLight)node).setConcentration( in.readFloat() );
-    }
-    
-    protected javax.media.j3d.SceneGraphObject createNode() {
-        return new SpotLight();
-    }
+		// issue 654: add missing attributes, since we should really inherit
+		// from PointLightState
+		Point3f point = new Point3f();
+		((PointLight) node).getAttenuation(point);
+		control.writePoint3f(out, point);
+		((PointLight) node).getPosition(point);
+		control.writePoint3f(out, point);
+
+		Vector3f dir = new Vector3f();
+		((SpotLight) node).getDirection(dir);
+		control.writeVector3f(out, dir);
+
+		out.writeFloat(((SpotLight) node).getSpreadAngle());
+		out.writeFloat(((SpotLight) node).getConcentration());
+	}
+
+	public void readObject(DataInput in) throws IOException {
+		
+		super.readObject(in);
+		
+		// issue 654: add missing attributes, since we should really inherit
+		// from PointLightState
+		if (control.getCurrentFileVersion() >= 5) {
+			((PointLight) node).setAttenuation(control.readPoint3f(in));
+			((PointLight) node).setPosition(control.readPoint3f(in));
+		}
+
+		((SpotLight) node).setDirection(control.readVector3f(in));
+		((SpotLight) node).setSpreadAngle(in.readFloat());
+		((SpotLight) node).setConcentration(in.readFloat());
+	}
+
+	protected javax.media.j3d.SceneGraphObject createNode() {
+		return new SpotLight();
+	}
 
 }
